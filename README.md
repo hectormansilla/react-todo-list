e6d97ccb21ce0018f218dc8c12e642f1d195822a
hectormansilla <hector@malware.cl>
2022-04-30 19:15:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tvlg4l
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tvlg4l)
---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Todo(props) {
+  const { todos, removeTodo, completeTodo } = props;
+  return todos.map((todo, index) => {
+    return (
+      <div
+        key={index}
+        className={todo.isCompleted ? 'todo-row complete' : 'todo-row'}
+      >
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+          {todo.text}
+        </div>
+        <button onClick={() => removeTodo(todo.id)}>Eliminar</button>
+      </div>
+    );
+  });
+}
+
+export default Todo;
